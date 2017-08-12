9f99325647ec6067e4071141f3be8c6aa77b0316
Kelby Ludwig <kelbyludwig@gmail.com>
2017-08-12 01:45:18 -0500

    updated filter loop to work with no filters again

--- reqdump.js
+++ reqdump.js
@@ -65,7 +65,6 @@ function requestLogger(options) {
     }
 
     function logger(url) {
-        var url = new URL(url);
         for (var i in filters) {
             url = filters[i](url);
         }
